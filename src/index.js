5ca78f9389747e9cf241df06cc3c42e074678349
Ant Man <34754995+aaronamendez@users.noreply.github.com>
2021-11-30 18:45:18 -0500

    Added a 3rd Event that uses wheel

--- src/index.js
+++ src/index.js
@@ -1,17 +1,25 @@
 import "./less/index.less";
 
 // Your code goes here!
+// 1
 const header = document.querySelector("header");
 function hoverColor(e) {
 	e.target.style.backgroundColor = "red";
 	// console.log(e);
 }
 header.addEventListener("mouseover", hoverColor);
-
+// 2
 const aTags = document.querySelectorAll("a");
 aTags.forEach((el) => {
 	el.addEventListener("click", function (e) {
 		e.preventDefault();
-		e.target.style.color = "blue";
+		e.target.style.color = "white";
 	});
 });
+
+const firstImg = document.querySelector(".intro img");
+function removeImg(e) {
+	e.target.src =
+		"https://image.shutterstock.com/image-vector/bus-isolated-260nw-541717828.jpg";
+}
+firstImg.addEventListener("wheel", removeImg);
